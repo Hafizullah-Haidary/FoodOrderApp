d221b6ec3afdb5c42cab2ea62f821fd8030c4d7e
Hafizullah-Haidary <hafiz_haidary@yahoo.com>
2022-05-30 06:03:12 +0430

    Create Angular App

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,21 @@
+// import { Component, OnInit } from '@angular/core';
+// import { FoodService } from '../services/food/food.service';
+// import { food } from '../shared/models/food';
+
+
+// @Component({
+//   selector: 'app-home',
+//   templateUrl: './home.component.html',
+//   styleUrls: ['./home.component.css']
+// })
+// export class HomeComponent implements OnInit {
+//   foods:food[]=[];
+//   currentRate:number=3;
+
+//   constructor(private fs:FoodService) { }
+
+//   ngOnInit(): void {
+//     this.foods=this.fs.getAll()
+//   }
+
+// }
